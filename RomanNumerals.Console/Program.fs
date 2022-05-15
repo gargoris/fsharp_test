a33ef9d00dbac33210e2a673ff8aaa87cf3350fb
Francisco Jose Fuentes Rodriguez <fran.fuentes@gmail.com>
2022-05-15 17:02:16 +0200

    Develop method

--- RomanNumerals.Console/Program.fs
+++ RomanNumerals.Console/Program.fs
@@ -1,2 +1,19 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open RomanNumerals.Library.Numerals
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+[<EntryPoint>]
+let Main (args) =
+    if args.Length <> 1 then
+        printfn "No values received"
+        0
+    else
+        let (parsed, value) = Int32.TryParse args[0]
+
+        if (not parsed) then
+            printfn "Error: expecting int as argument"
+            1
+        else
+            printfn $"Result for {args[0]}: {GetRomanNumeral value}"
+            0
--- RomanNumerals.Library/Romans.fs
+++ RomanNumerals.Library/Romans.fs
@@ -1,4 +1,44 @@
 ﻿namespace RomanNumerals.Library
 
+open System
+
+
 module Numerals =
-    let GetRomanNumeral (x: int) = "N"
+
+    let GetRomanNumeral (x: int) =
+        let thousands x =
+            let m = x / 1000
+            let rest = x % 1000
+            (String.replicate m "M", rest)
+
+        let baseChar (b: char) = int b - int '0'
+
+        let baseCase (maxLetter: string) midLetter lowerLetter x =
+            if x > 0 then
+
+                let roman =
+                    match x with
+                    | 9 -> lowerLetter + maxLetter
+                    | 4 -> lowerLetter + midLetter
+                    | _ when x > 4 -> midLetter + String.replicate (x - 5) lowerLetter
+                    | _ -> String.replicate x lowerLetter
+
+                roman
+            else
+                ""
+
+        let fiveHundreds = baseCase "M" "D" "C"
+        let fifties = baseCase "C" "L" "X"
+        let fifhs = baseCase "X" "V" "I"
+
+
+        let (num, rest) = thousands x
+        let processingString = rest.ToString("000")
+
+        let numD = processingString.[0] |> baseChar |> fiveHundreds
+
+        let numL = processingString.[1] |> baseChar |> fifties
+
+        let numF = processingString.[2] |> baseChar |> fifhs
+
+        num + numD + numL + numF
