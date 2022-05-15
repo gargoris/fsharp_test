567b4627b0ab18eff407ab9d580eab9df4aefc7e
Francisco Jose Fuentes Rodriguez <fran.fuentes@gmail.com>
2022-05-15 17:02:40 +0200

    Adding Tests

--- RomanNumerals.Tests/TestsRomanNumerals.fs
+++ RomanNumerals.Tests/TestsRomanNumerals.fs
@@ -1,12 +1,33 @@
 module TestsRomanNumerals
 
-open System
-open Xunit
 open RomanNumerals.Library.Numerals
+open Expecto
 
-[<Fact>]
-let ``Test numer one`` () =
-    let expected = "I"
+[<Tests>]
+let tests =
+    testList
+        "Test roman nummerals"
+        [ testCase "Testing number 1"
+          <| fun _ ->
+              let expected = "I"
+              let actual = GetRomanNumeral 1
 
-    let actual = GetRomanNumeral 1
-    Assert.Equal(expected, actual)
+              Expect.equal actual expected "Number 1 failed"
+          testCase "Testing number 1000"
+          <| fun _ ->
+              let expected = "M"
+              let actual = GetRomanNumeral 1000
+              Expect.equal actual expected "Number 1000 failed"
+          testCase "Testing number 1999"
+          <| fun _ ->
+              let expected = "MCMXCIX"
+              let actual = GetRomanNumeral 1999
+              Expect.equal actual expected "Number 1999 failed"
+
+          testCase "Testing number 1456"
+          <| fun _ ->
+              let expected = "MCDLVI"
+              let actual = GetRomanNumeral 1456
+              Expect.equal actual expected "Number 1456 failed"
+
+          ]
